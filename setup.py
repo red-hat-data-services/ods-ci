3067aea09e2c74dca7c7e44de5c675ee710129b3
Vasu Kulkarni <vasu@redhat.com>
2021-02-03 17:56:09 -0800

    Initial setup.py based on module requirements
    Signed-off-by: Vasu Kulkarni <vasu@redhat.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name="ods-ci",
+    version="0.1",
+    description="Red Hat Open Data Science QE Tier Tests",
+    author="ODS CI",
+    author_email="osd-ci@redhat.com",
+    install_requires=[
+        "reportportal-client",
+        "robot-framework",
+        "robotframework-seleniumlibrary",
+        "pytest",
+        "pytest-logger", 
+        "requests", 
+    ],
+    zip_safe=True,
+    include_package_data=True,
+    packages=find_packages(exclude=["ez_setup"]),
+)
+

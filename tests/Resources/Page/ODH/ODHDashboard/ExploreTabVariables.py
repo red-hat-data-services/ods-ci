aa89747aadfd5cacff64b9c46266b5f7f953c98a
bdattoma <bdattoma@redhat.com>
2022-01-11 18:24:11 +0100

    add python dictionary for explore tiles info
    Signed-off-by: bdattoma <bdattoma@redhat.com>

---
+++ tests/Resources/Page/ODH/ODHDashboard/ExploreTabVariables.py
@@ -0,0 +1,94 @@
+PARTNER_BADGE_TITLE = "Partner managed"
+SELF_BADGE_TITLE = "Self-managed"
+RH_BADGE_TITLE = "Red Hat managed"
+CMS_BADGE_TITLE = "Coming soon"
+BETA_BADGE_TITLE = "Beta"
+APPS_DICT = {
+     "anaconda-ce": {
+          "badges": [PARTNER_BADGE_TITLE],
+          "provider": "Anaconda",
+          "title": "Anaconda Commercial Edition",
+          "description": "Anaconda Commercial Edition is the most popular open source "
+                         "package distribution and management experience, optimized for commercial use."
+     },
+     "watson-studio": {
+          "badges": [SELF_BADGE_TITLE],
+          "provider": "IBM",
+          "title": "IBM Watson Studio",
+          "description": "IBM Watson Studio is a platform for embedding AI and machine "
+                         "learning into your business and creating custom models with your own data."
+
+     },
+     "aikit": {
+          "badges": [SELF_BADGE_TITLE],
+          "provider": "Intel",
+          "title": "Intel® oneAPI AI Analytics Toolkit Container",
+          "description": "The AI Kit is a set of AI software tools to accelerate "
+                         "end-to-end data science and analytics pipelines on Intel® architectures."
+
+     },
+     "jupyterhub": {
+          "badges": [SELF_BADGE_TITLE],
+          "provider": "IBM",
+          "title": "JupyterHub",
+          "description": "IBM Watson Studio is a platform for embedding AI and machine "
+                         "learning into your business and creating custom models with your own data."
+
+     },
+     "rhoam": {
+          "badges": [RH_BADGE_TITLE],
+          "provider": "Red Hat",
+          "title": "OpenShift API ManagementRed Hat certified and supported",
+          "description": "OpenShift API Management is a service that accelerates time-to-value "
+                         "and reduces the cost of delivering API-first, microservices-based applications."
+
+     },
+     "rhosak": {
+          "badges": [RH_BADGE_TITLE],
+          "provider": "Red Hat",
+          "title": "OpenShift Streams for Apache Kafka",
+          "description": "OpenShift Streams for Apache Kafka is a service for streaming data "
+                         "that reduces the cost and complexity of delivering real-time applications."
+
+     },
+     "openvino": {
+          "badges": [SELF_BADGE_TITLE],
+          "provider": "Intel",
+          "title": "OpenVINO",
+          "description": "OpenVINO is an open source toolkit to help optimize deep "
+                         "learning performance and deploy using an inference engine onto Intel hardware."
+
+     },
+     "pachyderm": {
+          "badges": [CMS_BADGE_TITLE],
+          "provider": "Pachyderm",
+          "title": "Pachyderm",
+          "description": "Pachyderm is a group of products that help data science teams "
+                         "automate and optimize their machine learning operations."
+
+     },
+     "perceptiLabs": {
+          "badges": [CMS_BADGE_TITLE],
+          "provider": "PercetiLabs",
+          "title": "PerceptiLabs",
+          "description": "PerceptiLabs is a visual modeling tool for editing, managing, "
+                         "and monitoring your machine learning models."
+
+     },
+     "seldon-deploy": {
+          "badges": [CMS_BADGE_TITLE],
+          "provider": "Seldon",
+          "title": "Seldon Deploy",
+          "description": "Seldon Deploy is a set of tools to simplify and accelerate "
+                         "the process of deploying and managing your machine learning models."
+
+     },
+     "starburst": {
+          "badges": [PARTNER_BADGE_TITLE, BETA_BADGE_TITLE],
+          "provider": "Starburst",
+          "title": "Starburst Galaxy",
+          "description": "Starburst Galaxy is a fully managed service to run high-performance "
+                         "queries across your various data sources using SQL."
+
+     },
+}

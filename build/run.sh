45e34e0bd2ab328c74987ae503ef20c1fa9795b6
bdattoma <bdattoma@redhat.com>
2022-04-04 16:15:17 +0200

    minor change

--- build/run.sh
+++ build/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo run script args "${RUN_SCRIPT_ARGS}"
-echo --extra-robot-args "${ROBOT_EXTRA_ARGS}"
+echo RUN SCRIPT ARGS: "${RUN_SCRIPT_ARGS}"
+echo ROBOT EXTRA ARGS: "${ROBOT_EXTRA_ARGS}"
 
 if [ -n "${ROBOT_EXTRA_ARGS}" ]; then \
         ./run_robot_test.sh --skip-pip-install ${RUN_SCRIPT_ARGS} --extra-robot-args "${ROBOT_EXTRA_ARGS}" ;\

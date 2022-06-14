f90920d5acb0b4ac1900798bde012f4e49dccfc7
bdattoma <bdattoma@redhat.com>
2022-06-14 17:28:33 +0200

    fix to work with any levels
    Signed-off-by: bdattoma <bdattoma@redhat.com>

--- utils/scripts/SplitSuite.py
+++ utils/scripts/SplitSuite.py
@@ -2,15 +2,14 @@
 # vim: et
 # example: SplitSuite.py:3:1 => divide whole suite into 3 parts and execute 1st part
 # from parts.
-# Author: Akash Shende
+# Based on Splitter authored by: Akash Shende https://github.com/akash0x53/robot-parallel/blob/master/SplitSuite.py
 
 from robot.api import SuiteVisitor
 from itertools import islice
 import math
-import sys
 
 
-MAX_SUITE_LEVELS = 3
+MAX_SUITE_LEVELS = 2
 
 
 def chunked(lst, nchunk):
@@ -25,7 +24,6 @@ def fetch_subsuites(parent):
 
 def fetch_subsuites_from_list(parents_list):
     children = []
-    # children += [fetch_subsuites(parent) for parent in parents_list]
     for parent in parents_list:
         children += fetch_subsuites(parent)
     return children
@@ -39,40 +37,40 @@ def fetch_suites_names(suites_list):
 class SplitSuite(SuiteVisitor):
     """Visitor that keeps only every Xth test in the visited suite structure."""
 
-    def __init__(self, parts, which_part=1):
+    def __init__(self, parts, which_part=1, max_suite_level=2):
         self.parts = float(parts)
         self.which_part = int(which_part)
+        self.max_suite_level = max_suite_level
 
     def visit_suite(self, suite):
         print("Visiting SUITE:", suite)
         sub_suites_list = []
-        if MAX_SUITE_LEVELS == 1:
+        if self.max_suite_level == 1:
             sub_suites_list = suite.suites
         else:
             for suite_lev_1 in suite.suites:
-                found = False
+                print("> Suite Lev 1: ", suite_lev_1.name)
                 next_lev_suites = fetch_subsuites(suite_lev_1)
-                for lev in range(2, MAX_SUITE_LEVELS+1):
-                    print("Extracting suites with level: ", lev)
-                    current_lev_suites = next_lev_suites
-                    print("Current Lev suites: ", current_lev_suites)
-                    next_lev_suites = fetch_subsuites_from_list(next_lev_suites)
-                    print("next level suites: ", next_lev_suites)
-                    if not next_lev_suites:
-                        print("Fetching sub suites from level: ", lev)
-                        print("fetched from lev2: ",fetch_subsuites_from_list(current_lev_suites))
-                        sub_suites_list += fetch_subsuites_from_list(current_lev_suites)
-                        found = True
-                if not found:
-                    sub_suites_list += fetch_subsuites(suite_lev_1)
-                print("\n")
+                for lev in range(2, self.max_suite_level+1):
+                    print(">>> Extracting suites with level: ", lev)
+                    current_lev_suites = next_lev_suites.copy()
+                    # print(">>> Suites: ", current_lev_suites)
+                    if lev == self.max_suite_level:
+                        sub_suites_list += current_lev_suites
+                        continue
+                    next_lev_suites = []
+                    for curr_lev_suite in current_lev_suites:
+                        next_lev_suites_tmp = fetch_subsuites(curr_lev_suite)
+                        if not next_lev_suites_tmp:
+                            # print("Fetching sub suites from previous level: ", str(lev-1))
+                            sub_suites_list.append(curr_lev_suite)
+                        else:
+                            next_lev_suites += next_lev_suites_tmp
 
         print("Total number of 1st level suites:", len(suite.suites))
         print("1st level suites:", suite.suites)
         print("Total number of fetched suites: ", len(sub_suites_list))
         suite_names = fetch_suites_names(sub_suites_list)
-        test_names = [t.name for t in sub_suites_list]
-        print("Test names in the suite: ", test_names)
         print("Suites names in the suite: ", suite_names)
         num_suite = math.ceil(len(sub_suites_list) / self.parts)
         print("Number of suites per split:", num_suite)

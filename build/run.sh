4baec322127b3dd89c66a00015048707b66e630a
bdattoma <bdattoma@redhat.com>
2022-07-29 14:53:12 +0200

    add ability to auto set test env

--- build/run.sh
+++ build/run.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 echo RUN SCRIPT ARGS: "${RUN_SCRIPT_ARGS}"
 echo ROBOT EXTRA ARGS: "${ROBOT_EXTRA_ARGS}"
+echo SET TEST ENVIRONMENT: "${SET_ENVIRONMENT}"
 
-if [ -n "${ROBOT_EXTRA_ARGS}" ]; then \
-        ./run_robot_test.sh --skip-pip-install ${RUN_SCRIPT_ARGS} --extra-robot-args "${ROBOT_EXTRA_ARGS}" ;\
-else \
-        ./run_robot_test.sh --skip-pip-install ${RUN_SCRIPT_ARGS};\
+# feature requested for MPS pipeline onboarding
+if [ "${SET_ENVIRONMENT}" -eq 1 ]; then \
+  if [ -z "${OC_HOST}" ]
+      then
+        echo -e "\033[0;33m You must set the OC_HOST env variable to automatically set the Test Environment for ODS-CI \033[0m"
+        exit 0
+      else
+        oc_host_no_port=$(echo $OC_HOST | sed 's/:[0-9]\+//g')
+        export RHODS_URL=${oc_host_no_port//api/rhods-dashboard-redhat-ods-applications}
+        export OCP_CONSOLE_URL=${oc_host_no_port//api/console-openshift-console}
+        ./install_idp.sh
+        yq --inplace '.OCP_CONSOLE_URL=env(OCP_CONSOLE_URL)' test-variables.yml
+        yq --inplace '.ODH_DASHBOARD_URL=env(RHODS_URL)' test-variables.yml
+        yq --inplace '.OCP_API_URL=env(OC_HOST)' test-variables.yml
+  fi
 fi
+./run_robot_test.sh --skip-pip-install ${RUN_SCRIPT_ARGS} --extra-robot-args "${ROBOT_EXTRA_ARGS}"
+
 

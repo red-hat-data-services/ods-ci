51a904c64df5c711e25349d992a18f5531b47495
bdattoma <bdattoma@redhat.com>
2022-07-29 15:24:06 +0200

    remove redundant steps

--- build/run.sh
+++ build/run.sh
@@ -11,13 +11,7 @@ if [ "${SET_ENVIRONMENT}" -eq 1 ]; then \
         exit 0
       else
         echo "-----| SET_ENVIRONMENT option is enabled. ODS-CI is going to configure the test environment for you..|-----\n"
-        oc_host_no_port=$(echo $OC_HOST | sed 's/:[0-9]\+//g')
-        export RHODS_URL=${oc_host_no_port//api/rhods-dashboard-redhat-ods-applications}
-        export OCP_CONSOLE_URL=${oc_host_no_port//api/console-openshift-console}
         ./install_idp.sh
-        yq --inplace '.OCP_CONSOLE_URL=env(OCP_CONSOLE_URL)' test-variables.yml
-        yq --inplace '.ODH_DASHBOARD_URL=env(RHODS_URL)' test-variables.yml
-        yq --inplace '.OCP_API_URL=env(OC_HOST)' test-variables.yml
   fi
 fi
 echo "-----| ODS-CI is start running the tests...|-----\n"

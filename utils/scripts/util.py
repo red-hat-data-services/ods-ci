edb0117bcef9d9ec0861fb00b2c349455841e00b
mwaykole <mwaykole@redhat.com>
2023-02-17 22:07:23 +0530

    log Improvement for ocm.py
    Signed-off-by: mwaykole <mwaykole@redhat.com>

--- utils/scripts/util.py
+++ utils/scripts/util.py
@@ -74,7 +74,7 @@ def execute_command(cmd, get_stderr=False, stderrPrint=False):
 
         output = process.stdout
         if stderrPrint and process.stderr:
-            print("ods-ci-Error:{}".format(stderrPrint.stderr, end=""))
+            print("ods-ci-Error:{}".format(process.stderr, end=""))
 
         if get_stderr:
             err = process.stderr

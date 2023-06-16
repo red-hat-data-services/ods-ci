d6b21a5a7974fce39d507c2f81c68a4c288ab93e
Luca Giorgi <lgiorgi@redhat.com>
2023-06-16 10:32:31 +0200

    update command in generatetestconfigfile.py
    Signed-off-by: Luca Giorgi <lgiorgi@redhat.com>

--- ods_ci/utils/scripts/testconfig/generateTestConfigFile.py
+++ ods_ci/utils/scripts/testconfig/generateTestConfigFile.py
@@ -98,7 +98,7 @@ def get_prometheus_token(project):
     """
     Get prometheus token for the cluster.
     """
-    cmd = "oc create token prometheus -n {}".format(project)
+    cmd = "oc create token prometheus -n {} --duration 6h".format(project)
     prometheus_token = execute_command(cmd)
     return prometheus_token.strip("\n")
 

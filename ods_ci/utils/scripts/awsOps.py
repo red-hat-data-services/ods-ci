2949027ec6f60105ccbeaec97c834b8fa11be3bd
mwaykole <mwaykole@redhat.com>
2023-07-17 10:38:46 +0530

    Rosa cluster Creation
    Signed-off-by: mwaykole <mwaykole@redhat.com>

--- ods_ci/utils/scripts/awsOps.py
+++ ods_ci/utils/scripts/awsOps.py
@@ -1,9 +1,10 @@
-
 from util import (
     execute_command,
 )
 from logging import log
 from time import sleep
+
+
 def aws_configure(aws_access_key_id, aws_secret_access_key, aws_region):
     """
     Runs aws configure and set the configuration required
@@ -11,25 +12,36 @@ def aws_configure(aws_access_key_id, aws_secret_access_key, aws_region):
     """
     print(aws_access_key_id, aws_secret_access_key, aws_region)
 
-    cmd_aws_configure_key_id = ["aws", "configure", "set" ,"default.aws_access_key_id" ,aws_access_key_id]
-    print(' '.join(cmd_aws_configure_key_id))
-    ret = execute_command(' '.join(cmd_aws_configure_key_id))
+    cmd_aws_configure_key_id = [
+        "aws",
+        "configure",
+        "set",
+        "default.aws_access_key_id",
+        aws_access_key_id,
+    ]
+    print(" ".join(cmd_aws_configure_key_id))
+    ret = execute_command(" ".join(cmd_aws_configure_key_id))
     if ret is None:
         print("Failed  to configure aws_access_key_id")
-        return ret 
+        return ret
     sleep(1)
 
-    cmd_aws_configure_access_id = ["aws", "configure", "set" ,"default.aws_secret_access_key" ,aws_secret_access_key]
-    print(' '.join(cmd_aws_configure_access_id))
-    ret = execute_command(' '.join(cmd_aws_configure_access_id))
+    cmd_aws_configure_access_id = [
+        "aws",
+        "configure",
+        "set",
+        "default.aws_secret_access_key",
+        aws_secret_access_key,
+    ]
+    print(" ".join(cmd_aws_configure_access_id))
+    ret = execute_command(" ".join(cmd_aws_configure_access_id))
     if ret is None:
         print("Failed  to configure aws_secret_access_key")
-        return ret     
+        return ret
     sleep(1)
 
-    cmd_aws_configure_region = ["aws", "configure", "set" ,"default.region" ,aws_region]
-    print(' '.join(cmd_aws_configure_region))
-    ret = execute_command(' '.join(cmd_aws_configure_region))
+    cmd_aws_configure_region = ["aws", "configure", "set", "default.region", aws_region]
+    ret = execute_command(" ".join(cmd_aws_configure_region))
     if ret is None:
         print("Failed  to configure region")
-        return ret     
+        return ret
--- ods_ci/utils/scripts/rosa/rosa.py
+++ ods_ci/utils/scripts/rosa/rosa.py
@@ -1,14 +1,14 @@
-import argparse,sys,os
+import argparse, sys, os
+
 dir_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(dir_path + "/../")
 from logger import log
 from awsOps import aws_configure
 from rosaOps import (
-    rosa_create_cluster, 
+    rosa_create_cluster,
     create_account_roles,
-    wait_for_osd_cluster_to_be_ready
-    )
-
+    wait_for_osd_cluster_to_be_ready,
+)
 
 
 class RosaClusterManager:
@@ -19,25 +19,40 @@ class RosaClusterManager:
         self.profile = args.get("profile")
         self.cluster_name = args.get("cluster_name")
         self.compute_nodes = args.get("compute_nodes")
-        self.compute_machine_type =args.get("compute_machine_type")
+        self.compute_machine_type = args.get("compute_machine_type")
         self.rosa_version = args.get("rosa_version")
-        self.channel_name  = args.get("channel_name")
+        self.channel_name = args.get("channel_name")
 
     def create_rosa_cluster(self):
-        print( self.cluster_name, self.aws_region ,self.channel_name, self.compute_nodes,
-              self.compute_machine_type, self.rosa_version)
-        aws_configure(self.aws_access_key_id, self.aws_secret_access_key, self.aws_region)
+        print(
+            self.cluster_name,
+            self.aws_region,
+            self.channel_name,
+            self.compute_nodes,
+            self.compute_machine_type,
+            self.rosa_version,
+        )
+        aws_configure(
+            self.aws_access_key_id, self.aws_secret_access_key, self.aws_region
+        )
         create_account_roles()
         rosa_create_cluster(
-            self.cluster_name, self.aws_region, self.channel_name,
-            self.compute_nodes, self.compute_machine_type, self.rosa_version)
+            self.cluster_name,
+            self.aws_region,
+            self.channel_name,
+            self.compute_nodes,
+            self.compute_machine_type,
+            self.rosa_version,
+        )
         wait_for_osd_cluster_to_be_ready(self.cluster_name)
 
-def main():
 
-    parser = argparse.ArgumentParser(description="Create a ROSA cluster",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    
+def main():
+    parser = argparse.ArgumentParser(
+        description="Create a ROSA cluster",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
     # Argument parsers for create_cluster
     subparsers = parser.add_subparsers(
         title="Available sub commands", help="sub-command help"
@@ -46,75 +61,75 @@ def main():
         "create_rosa_cluster",
         help=("create ROSA clusters using openshift installer"),
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )   
+    )
     rosaClusterCreate_parser.add_argument(
         "--aws-access-key-id",
         required=True,
         action="store",
         dest="aws_access_key_id",
-        help="AWS access key ID")
-    
+        help="AWS access key ID",
+    )
+
     rosaClusterCreate_parser.add_argument(
-        "--aws-secret-access-key", 
+        "--aws-secret-access-key",
         required=True,
         action="store",
         dest="aws_secret_access_key",
-        help="AWS secret access key"
-        )
-    
+        help="AWS secret access key",
+    )
+
     rosaClusterCreate_parser.add_argument(
         "--aws_region",
         required=True,
         action="store",
-        dest="aws_region" ,        
-        help="AWS aws_region"
-        )
-    
+        dest="aws_region",
+        help="AWS aws_region",
+    )
+
     rosaClusterCreate_parser.add_argument(
-        "--cluster-name", 
-        required=True, 
+        "--cluster-name",
+        required=True,
         action="store",
         dest="cluster_name",
-        help="ROSA cluster name"
-        )
-    
+        help="ROSA cluster name",
+    )
+
     rosaClusterCreate_parser.add_argument(
-        "--compute_nodes", 
-        required=True, 
+        "--compute_nodes",
+        required=True,
         action="store",
         dest="compute_nodes",
-        help="Number of compute nodes")
-    
+        help="Number of compute nodes",
+    )
+
     rosaClusterCreate_parser.add_argument(
-        "--compute-machine-type", 
-        required=True, 
+        "--compute-machine-type",
+        required=True,
         action="store",
         dest="compute_machine_type",
-        help="Compute machine type"
-        )
-    
+        help="Compute machine type",
+    )
+
     rosaClusterCreate_parser.add_argument(
-        "--osd-version", 
+        "--osd-version",
         required=True,
         action="store",
         dest="rosa_version",
-        help="ROSA version"
-        )
+        help="ROSA version",
+    )
     rosaClusterCreate_parser.add_argument(
-        "--channel-name", 
+        "--channel-name",
         required=True,
         action="store",
         dest="channel_name",
-        help="Channel Group Stable/Candidate"
-        )
+        help="Channel Group Stable/Candidate",
+    )
     rosa_cluster_manager = RosaClusterManager()
 
-    rosaClusterCreate_parser.set_defaults(
-        func=rosa_cluster_manager.create_rosa_cluster)
+    rosaClusterCreate_parser.set_defaults(func=rosa_cluster_manager.create_rosa_cluster)
     args = parser.parse_args(namespace=rosa_cluster_manager)
     if hasattr(args, "func"):
         args.func()
-    
 
 
 if __name__ == "__main__":

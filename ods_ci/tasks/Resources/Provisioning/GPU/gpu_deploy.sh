734a405e328865847f91ab2e0d312bd50d5d7396
Fede Alonso <fealonso@redhat.com>
2023-10-20 17:20:23 +0200

    Add a rerun migration for accelerators in gpu deploy script

--- ods_ci/tasks/Resources/Provisioning/GPU/gpu_deploy.sh
+++ ods_ci/tasks/Resources/Provisioning/GPU/gpu_deploy.sh
@@ -40,9 +40,54 @@ function wait_until_gpu_pods_are_running() {
 
 }
 
+function rerun_accelerator_migration() {
+#As we are adding the GPUs after installing the operator, those GPUs are not discovered automatically.
+#In order to rerun the migration we need to
+#1. Delete the migration configmap
+#2. Delete the dashboard replicaset to trigger new pods
+#Context: https://github.com/opendatahub-io/odh-dashboard/issues/1938
+
+  local timeout_seconds=600
+  local sleep_time=5
+
+  echo "Deleting configmap migration-gpu-status"
+  oc delete configmap migration-gpu-status -n redhat-ods-applications
+  if [[ $? -ne 0 ]]
+    then
+      printf "ERROR: When trying to delete the migration-gpu-status configmap\n"
+      return 1
+  fi
+
+  dashboard_rs=$(oc get rs -n redhat-ods-applications | grep rhods-dashboard- | awk '{print $1;exit}')
+  echo "Deleting ReplicaSet $dashboard_rs"
+  oc delete rs $dashboard_rs  -n redhat-ods-applications
+  if [[ $? -ne 0 ]]
+    then
+      printf "ERROR: When trying to delete the dashboard replica set\n"
+      return 1
+  fi
+
+  # Wait until all dashboard pods are ready again
+  SECONDS=0
+  while [ "$SECONDS" -le "$timeout_seconds" ]; do
+    dashboard_pods=$(oc get deployment rhods-dashboard -n redhat-ods-applications | grep rhods-dashboard | awk '{print $2;exit}')
+    dashboard_pods_total=`echo $dashboard_pods | cut -c3-3`
+    dashboard_pods_avail=`echo $dashboard_pods | cut -c1-1`
+    ((remaining_seconds = timeout_seconds - SECONDS))
+    echo "Dashboard pods: Available $dashboard_pods_avail out of $dashboard_pods_total ... (timeout in $remaining_seconds seconds)"
+    if [ $dashboard_pods_avail == $dashboard_pods_total ]; then
+      break
+    else
+      sleep $sleep_time
+      ((SECONDS+=$sleep_time))
+    fi
+  done
+}
+
 wait_until_gpu_pods_are_running
 oc apply -f ${GPU_INSTALL_DIR}/nfd_deploy.yaml
 oc get csv -n nvidia-gpu-operator $CSVNAME -ojsonpath={.metadata.annotations.alm-examples} | jq .[0] > clusterpolicy.json
 oc apply -f clusterpolicy.json
+rerun_accelerator_migration
 
 

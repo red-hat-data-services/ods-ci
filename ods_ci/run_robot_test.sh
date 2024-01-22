5322277e32af50d33acb68105257a8d331548375
Jan Stourac <jstourac@redhat.com>
2024-01-22 15:57:05 +0100

    Print more info for the pre-used venv usage (#1136)
    * Print more info for the pre-used venv usage
    
    Let's be a bit more clear whether we're using the pre-created venv or
    not.
    
    * Fix some typos and improve phrasing
    
    ---------
    
    Co-authored-by: Jiri Daněk <jdanek@redhat.com>

--- ods_ci/run_robot_test.sh
+++ ods_ci/run_robot_test.sh
@@ -314,12 +314,15 @@ if command -v yq &> /dev/null
 fi
 
 if [[ ${SKIP_INSTALL} -eq 0 ]]; then
-  # use a pre-created virtual environment if it exists
   virtenv="${HOME}/.local/ods-ci/.venv"
   if [[ -d "${virtenv}" ]]; then
+    echo "Using a pre-created virtual environment in '${virtenv}' for poetry to save time."
     poetry config --local virtualenvs.in-project true
     ln --symbolic "${virtenv}" ./.venv
+  else
+    echo "Pre-created virtual environment has not been found in '${virtenv}'. All dependencies will be installed from scratch."
   fi
+
   poetry --no-interaction install --sync
 fi
 # shellcheck disable=SC1091

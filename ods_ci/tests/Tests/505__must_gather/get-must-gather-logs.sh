77287f2150b3a8d530fc229a17afc5e4219edd8d
Jan Stourac <jstourac@redhat.com>
2024-05-04 13:08:57 +0200

    [fix] Update image used for must-gather test
    A new image was build accidentally a couple of days ago. This image
    should contain same code as the previous one, but since it is build
    recently, multiple packages from base image are updated.
    
    Also, since this image has been marked for 2.9.0 already and is tagged
    as stable, we should start to use it in our tests too [1,2].
    
    [1] https://github.com/red-hat-data-services/rhoai-disconnected-install-helper/blob/main/rhoai-2.9.md
    [2] https://access.redhat.com/solutions/7061604

--- ods_ci/tests/Tests/505__must_gather/get-must-gather-logs.sh
+++ ods_ci/tests/Tests/505__must_gather/get-must-gather-logs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Redirecting stdout/stderr of must-gather to a file, as it fills up the
 # process buffer and prevents the script from running further.
-oc adm must-gather --image=quay.io/modh/must-gather@sha256:c2d780156a0e7cec975c9c150bee00b1facb8f6213e7b98a7a489448d76dfd94 &> must-gather-results.txt
+oc adm must-gather --image=quay.io/modh/must-gather@sha256:5dd8b6f7e72c7fb3a5b46a48304514e4975fd6ed171cc3b1386771151020110a &> must-gather-results.txt
 
 if [ $? -eq 0 ]
 then

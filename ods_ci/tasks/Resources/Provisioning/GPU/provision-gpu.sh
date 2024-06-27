42bada7f8b32bc330021402f69fb25b461921e9e
bdattoma <bdattoma@redhat.com>
2024-06-27 17:39:01 +0200

    generalize GPU script away from AWS provider

--- ods_ci/tasks/Resources/Provisioning/GPU/provision-gpu.sh
+++ ods_ci/tasks/Resources/Provisioning/GPU/provision-gpu.sh
@@ -3,6 +3,9 @@ set -e
 
 # Optional params
 INSTANCE_TYPE=${1:-"g4dn.xlarge"}
+PROVIDER=${2:-"AWS"}
+KUSTOMIZE_PATH="$PWD/ods_ci/tasks/Resources/Provisioning/Hive/GPU"
+MACHINESET_PATH="$KUSTOMIZE_PATH/base/source-machineset.yaml"
 
 # Check if existing machineset GPU already exists
 EXISTING_GPU_MACHINESET="$(oc get machineset -n openshift-machine-api -o jsonpath="{.items[?(@.metadata.annotations['machine\.openshift\.io/GPU']>'0')].metadata.name}")"
@@ -14,28 +17,18 @@ fi
 
 # Select the first machineset as a template for the GPU machineset
 SOURCE_MACHINESET=$(oc get machineset -n openshift-machine-api -o name | head -n1)
+oc get -o yaml -n openshift-machine-api $SOURCE_MACHINESET  > $MACHINESET_PATH
 
-# Reformat with jq, for better diff result.
-oc get -o json -n openshift-machine-api $SOURCE_MACHINESET  | jq -r > /tmp/source-machineset.json
-
-OLD_MACHINESET_NAME=$(jq '.metadata.name' -r /tmp/source-machineset.json )
+# rename machine set in the template file
+OLD_MACHINESET_NAME=$(yq '.metadata.name' $MACHINESET_PATH )
 NEW_MACHINESET_NAME=${OLD_MACHINESET_NAME/worker/gpu}
+sed -i'' -e "s/$OLD_MACHINESET_NAME/$NEW_MACHINESET_NAME/g" $MACHINESET_PATH
 
+# set the desired node flavor in the kustomize overlay
+sed -i'' -e "s/INSTANCE_TYPE/$INSTANCE_TYPE/g" $KUSTOMIZE_PATH/overlays/$PROVIDER/gpu.yaml
 
-# Change instanceType and delete some stuff
-jq -r --arg INSTANCE_TYPE "$INSTANCE_TYPE" '.spec.template.spec.providerSpec.value.instanceType=$INSTANCE_TYPE
-  | del(.metadata.selfLink)
-  | del(.metadata.uid)
-  | del(.metadata.creationTimestamp)
-  | del(.metadata.resourceVersion)
-' /tmp/source-machineset.json > /tmp/gpu-machineset.json
-
-# Change machineset name
-sed -i'' -e "s/$OLD_MACHINESET_NAME/$NEW_MACHINESET_NAME/g" /tmp/gpu-machineset.json
-# Create new machineset
-oc apply -f /tmp/gpu-machineset.json
-rm /tmp/source-machineset.json
-rm /tmp/gpu-machineset.json
+# create the new MachineSet using kustomize
+oc apply --kustomize $KUSTOMIZE_PATH/overlays/$PROVIDER
 
 # Add GPU label to the new machine-set
 oc patch machinesets -n openshift-machine-api "$NEW_MACHINESET_NAME" -p '{"metadata":{"labels":{"gpu-machineset":"true"}}}' --type=merge

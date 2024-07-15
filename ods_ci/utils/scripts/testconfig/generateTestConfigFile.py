13a374e42e8eb993d0f034aa2a9ae8fc5601b765
bdattoma <bdattoma@redhat.com>
2024-07-15 17:44:24 +0200

    fix formatting

--- ods_ci/utils/scripts/testconfig/generateTestConfigFile.py
+++ ods_ci/utils/scripts/testconfig/generateTestConfigFile.py
@@ -174,8 +174,10 @@ def get_dashboard_url():
     """
     Get dashboard url for the open data science.
     """
-    cmd = "(oc get route -n opendatahub-operators -o json  | jq '.items[].spec.host' | grep odh-dashboard) || " + \
-          "(oc get route -n redhat-ods-applications -o json  | jq '.items[].spec.host' | grep rhods-dashboard)"
+    cmd = (
+        "(oc get route -n opendatahub-operators -o json  | jq '.items[].spec.host' | grep odh-dashboard) || "
+        + "(oc get route -n redhat-ods-applications -o json  | jq '.items[].spec.host' | grep rhods-dashboard)"
+    )
     dashboard_url = execute_command(cmd)
     return "https://" + dashboard_url.strip('"').strip("\n")
 

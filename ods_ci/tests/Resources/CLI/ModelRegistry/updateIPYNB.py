ee850b397b66aa99961e96501fa8454c84cf5026
tonyxrmdavidson <tonyxrmdavidson@yahoo.co.uk>
2024-07-29 19:12:14 +0100

    More python linting fixes
    Signed-off-by: tonyxrmdavidson <tonyxrmdavidson@yahoo.co.uk>

--- ods_ci/tests/Resources/CLI/ModelRegistry/updateIPYNB.py
+++ ods_ci/tests/Resources/CLI/ModelRegistry/updateIPYNB.py
@@ -2,19 +2,21 @@ import json
 import subprocess
 
 # Define the filenames
-input_filename = 'tests/Resources/CLI/ModelRegistry/MRMS_TEST_RUNNER.ipynb'
-output_filename = 'tests/Resources/CLI/ModelRegistry/MRMS_UPDATED.ipynb'
+input_filename = "tests/Resources/CLI/ModelRegistry/MRMS_TEST_RUNNER.ipynb"
+output_filename = "tests/Resources/CLI/ModelRegistry/MRMS_UPDATED.ipynb"
+
 
 # Function to run an oc command and retrieve output
 def get_oc_command_output(command):
     try:
-        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        result = subprocess.run(command, check=True, capture_output=True, text=True)
         return result.stdout.strip()
     except subprocess.CalledProcessError as e:
-        raise RuntimeError(f"Failed to run oc command: {e}\n{e.stderr}")
+        raise RuntimeError(f"Failed to run oc command: {e}\n{e.stderr}") from e
+
 
 # Run the oc command to get the domain
-command = ['oc', 'get', 'ingresses.config/cluster', '-o', 'jsonpath={.spec.domain}']
+command = ["oc", "get", "ingresses.config/cluster", "-o", "jsonpath={.spec.domain}"]
 replacement_text = get_oc_command_output(command)
 
 # Check if the replacement text was successfully retrieved
@@ -24,19 +26,19 @@ if not replacement_text:
 print(f"Replacement text (DOMAIN) is: {replacement_text}")
 
 # Read the notebook file
-with open(input_filename, 'r') as file:
+with open(input_filename, "r") as file:
     notebook = json.load(file)
 
 # Replace placeholder text in code cells
-placeholder = 'PLACEHOLDER'
-for cell in notebook.get('cells', []):
-    if cell.get('cell_type') == 'code':
-        source = cell.get('source', [])
+placeholder = "PLACEHOLDER"
+for cell in notebook.get("cells", []):
+    if cell.get("cell_type") == "code":
+        source = cell.get("source", [])
         # Replace the placeholder text with the replacement text
-        cell['source'] = [line.replace(placeholder, replacement_text) for line in source]
+        cell["source"] = [line.replace(placeholder, replacement_text) for line in source]
 
 # Save the modified notebook
-with open(output_filename, 'w') as file:
+with open(output_filename, "w") as file:
     json.dump(notebook, file, indent=2)
 
 print(f"Modified notebook saved as {output_filename}")

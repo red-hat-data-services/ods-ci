7cbe9d4c62ac0564f07b3ce7ee6ad2a2b5804343
César Francisco San Nicolás Martínez <csannico@redhat.com>
2024-08-14 15:39:41 +0200

    fix: solve must gather test issues

--- ods_ci/tests/Resources/CLI/MustGather/get-must-gather-logs.sh
+++ ods_ci/tests/Resources/CLI/MustGather/get-must-gather-logs.sh
@@ -3,6 +3,8 @@
 # process buffer and prevents the script from running further.
 oc adm must-gather --image=quay.io/modh/must-gather@sha256:1bd8735d715b624c1eaf484454b0d6d400a334d8cbba47f99883626f36e96657 &> must-gather-results.txt
 
+cat must-gather-results.txt
+
 if [ $? -eq 0 ]
 then
     echo "SUCCESS: must-gather logs can be found in repo must-gather-local.*"

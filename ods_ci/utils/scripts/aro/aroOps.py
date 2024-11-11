be4ac9df61d18bcf15ccf4bfa6034fb579e5e0a3
Lena Horsley <lhorsley@lhorsley-thinkpadt14sgen2i.rht.csb>
2024-11-11 11:59:38 -0500

    test automation

---
+++ ods_ci/utils/scripts/aro/aroOps.py
@@ -0,0 +1,141 @@
+import sys
+from time import sleep
+import re
+
+from ods_ci.utils.scripts.logger import log
+from ods_ci.utils.scripts.util import execute_command
+
+
+# function/method to get the available Azure/ARO versions
+def get_aro_version(version) -> str | None:
+
+    char_to_count = "."
+
+    if version.count(char_to_count) == 1:
+        version = version + "."
+
+    get_versions_cmd=(f"az aro get-versions -l eastus | grep {version}")
+    final_list = []
+
+    ret = execute_command(get_versions_cmd)
+
+    version_string = re.sub("[\"\']", "", ret)
+    version_string = version_string.lstrip(' ').replace("\n","").replace(",","")
+    version_list = version_string.split()
+
+    for my_string in version_list:
+        if version in my_string:
+            final_list.append(my_string)
+
+    if len(final_list) > 0:
+        return final_list[-1]
+    else:
+        log.error("INVALID OCP VERSION FOR ARO CLUSTER: ", version)
+        log.error("Versions available:")
+        execute_command("az aro get-versions -l eastus")
+        return None
+        # exit out and the cluster build will fail
+         # this is expected/wanted behavior
+    
+
+# function/method for cli login
+def aro_cli_login(aro_client_id, aro_tenant_id, aro_secret_pwd):
+
+    aro_cli_login_cmd=(f"az login --service-principal -u {aro_client_id} -p {aro_secret_pwd} --tenant {aro_tenant_id}")
+
+    ret = execute_command(aro_cli_login_cmd)
+    if "ERROR" in ret:
+        log.error("LOGIN UNSUCCESSFUL")
+        log.error("Invalid tenant id, client it and/or secret")
+    else:
+        print("LOGIN SUCCESSFUL")
+        # now, check the json. pick a few fields that will let you know if the login was successful
+
+
+def execute_terraform(cluster_name, subscription_id, version):
+    print(">>>>> Here is the cluster name again: ", cluster_name)
+    print(">>>>> Here is the version: ", version)
+
+    # pull_secret_path="~/Downloads/pull-secret.txt"
+    execute_command(f"terraform init && terraform plan -out tf.plan -var=subscription_id={subscription_id} -var=cluster_name={cluster_name} -var=aro_version={version} && terraform apply tf.plan")
+
+
+def get_aro_cluster_info(my_cluster_name):
+    # resource_group = my_cluster_name + "-rg"
+
+    api_server_profile_url = get_cluster_info_field_value(my_cluster_name, "apiserverProfile.url")
+    console_profile_url = get_cluster_info_field_value(my_cluster_name, "consoleProfile.url")
+    cluster_profile_version =  get_cluster_info_field_value(my_cluster_name, "clusterProfile.version")
+    provisioning_state = get_cluster_info_field_value(my_cluster_name, "provisioningState")
+    cluster_location =  get_cluster_info_field_value(my_cluster_name, "location") 
+
+    if provisioning_state == "Succeeded":
+        print("cluster is up and running")
+    else:
+        print("what happened")
+
+    print("Cluster name: ", my_cluster_name)
+    print("Provisioning status: ", provisioning_state)
+    print("Cluster location: ", cluster_location)
+    print("Version: ", cluster_profile_version)
+    print("Console URL: ", console_profile_url)
+    print("API URL: ", api_server_profile_url)
+
+
+def aro_cluster_login(my_cluster_name):
+    resource_group = my_cluster_name + "-rg"
+    print("Obtain cluster credentials...")
+    api_server_profile_url = get_cluster_info_field_value(my_cluster_name, "apiserverProfile.url")
+
+    aro_cluster_pwd = execute_command(f"az aro list-credentials --name {my_cluster_name} --resource-group {resource_group} -o tsv --query kubeadminPassword")
+
+    print("Login to the cluster...")
+    execute_command(f"oc login -u kubeadmin -p {aro_cluster_pwd} {api_server_profile_url} --insecure-skip-tls-verify=true")
+
+    print("")
+    print("Check the cluster nodes...")
+    execute_command(f"oc get nodes")
+
+    print("")
+    print("Check the cluster operators...")
+    execute_command(f"oc get co; oc get clusterversion")
+    print("")
+
+
+def aro_cluster_delete(cluster_name):
+
+    # $ ARO_PASS=$(az aro list-credentials --name ${TF_VAR_cluster_name} --resource-group ${TF_VAR_cluster_name}-rg  -o tsv --query kubeadminPassword)
+    # $ oc login $(terraform output -raw api_url) --username kubeadmin --password "${ARO_PASS}" --insecure-skip-tls-verify=true
+    # oc get nodes
+    # oc get co; oc get clusterversion
+    resource_group = cluster_name + "-rg"
+    provisioning_state = get_cluster_info_field_value(cluster_name, "provisioningState")
+    
+    provisioning_state_check = provisioning_state.strip()
+
+    time_count = 0
+    if provisioning_state_check == "Succeeded":
+        print("Deleting cluster: ", cluster_name)
+        execute_command(f"az aro delete --name {cluster_name} --resource-group {resource_group} --yes -y --no-wait")
+        delete_provisioning_state = get_cluster_info_field_value(cluster_name, "provisioningState")
+        while delete_provisioning_state == "Deleting" and time_count < 3600:
+            print(delete_provisioning_state)
+            sleep(60)
+            time_count += 60
+            delete_provisioning_state = get_cluster_info_field_value(cluster_name, "provisioningState")
+        if "ERROR: (ResourceNotFound)" in delete_provisioning_state:
+            print("Cluster has been successfully deleted")
+        elif time_count >= 3600:
+            print("Time exceeded for cluster deletion. Please delete the cluster manually")
+
+    else:
+        print("Cannot find cluster. Check for the cluster and delete manually if present.")
+
+    
+def get_cluster_info_field_value(my_cluster_name, cluster_info_field):
+    resource_group = my_cluster_name + "-rg"
+
+    my_command_output = execute_command(f"az aro show --name {my_cluster_name} --resource-group {resource_group} | jq '.{cluster_info_field}'")
+    my_command_output = re.sub("[\"\']", "", my_command_output)
+
+    return my_command_output.strip()
---
+++ ods_ci/utils/scripts/aro/aro_ocm.py
@@ -0,0 +1,200 @@
+import sys
+import argparse
+from time import sleep
+import re
+
+from aroOps import (
+    get_aro_version,
+    aro_cli_login,
+    execute_terraform,
+    get_aro_cluster_info,
+    aro_cluster_login,
+    aro_cluster_delete
+)
+
+from ods_ci.utils.scripts.logger import log
+from ods_ci.utils.scripts.util import execute_command
+
+class AroClusterManager:
+    def __init__(self, args={}):
+        self.aro_client_id = args.get("aro_client_id")
+        self.aro_tenant_id = args.get("aro_tenant_id")
+        self.aro_secret_pwd = args.get("aro_secret_pwd")
+        self.aro_ocp_version = args.get("aro_ocp_version")
+        self.aro_cluster_name = args.get("aro_cluster_name")
+        self.aro_subscription_id = args.get("aro_subscription_id")
+
+
+    def create_aro_cluster(self):
+        print("Name of cluster to be created", self.aro_cluster_name)
+        aro_cli_login(self.aro_client_id, self.aro_tenant_id, self.aro_secret_pwd)
+        my_version = get_aro_version(self.aro_ocp_version)
+        print("OCP version selected for ARO cluster: ", my_version)
+        execute_terraform(self.aro_cluster_name, self.aro_subscription_id, str(my_version))
+        get_aro_cluster_info(self.aro_cluster_name)
+        aro_cluster_login(self.aro_cluster_name)
+
+    def delete_aro_cluster(self):
+        print("Name of cluster to be deleted", self.aro_cluster_name)
+        aro_cli_login(self.aro_client_id, self.aro_tenant_id, self.aro_secret_pwd)
+        get_aro_cluster_info(self.aro_cluster_name)
+        aro_cluster_delete(self.aro_cluster_name)
+        print("It looks like something disappeared.")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="ARO Cluster Actions",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    
+
+    # Argument parsers for  ARO cluster
+    subparsers = parser.add_subparsers(title="Available sub commands", help="Available sub commands")
+    aro_create_cluster_parser = subparsers.add_parser(
+        "create_aro_cluster",
+        help="create ARO clusters",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    # delete_subparsers = parser.add_subparsers(title="Available sub commands", help="sub-command help")
+    aro_delete_cluster_parser = subparsers.add_parser(
+        "delete_aro_cluster",
+        help="delete ARO clusters",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+
+    # aro_cluster_parser.add_argument(
+    #     "--my-create-phrase",
+    #     required=True,
+    #     action="store",
+    #     dest="my_create_phrase",
+    #     help="My Create Phrase",
+    # )
+
+    aro_create_cluster_parser.add_argument(
+        "--aro-cluster-name",
+        required=True,
+        action="store",
+        dest="aro_cluster_name",
+        help="ARO Cluster Name",
+    )
+
+    aro_create_cluster_parser.add_argument(
+        "--aro-client-id",
+        required=True,
+        action="store",
+        dest="aro_client_id",
+        help="ARO Client ID",
+    )
+
+    aro_create_cluster_parser.add_argument(
+        "--aro-tenant-id",
+        required=True,
+        action="store",
+        dest="aro_tenant_id",
+        help="ARO Tenant ID",
+    )
+
+    aro_create_cluster_parser.add_argument(
+        "--aro-subscription-id",
+        required=True,
+        action="store",
+        dest="aro_subscription_id",
+        help="ARO Subscription ID",
+    )
+
+    aro_create_cluster_parser.add_argument(
+        "--aro-secret-pwd",
+        required=True,
+        action="store",
+        dest="aro_secret_pwd",
+        help="ARO Secret PWD",
+    )
+
+    aro_create_cluster_parser.add_argument(
+        "--aro-ocp-version",
+        required=True,
+        action="store",
+        dest="aro_ocp_version",
+        help="ARO OCP Version",
+    )
+
+    # aro_create_cluster_parser.add_argument(
+    #     "--aro-pull-secret-path",
+    #     required=True,
+    #     action="store",
+    #     dest="aro_pull_secret_path",
+    #     help="ARO pull secret path",
+    # )
+
+    # aro_cluster_parser.add_argument(
+    #     "--my-delete-phrase",
+    #     required=True,
+    #     action="store",
+    #     dest="my_delete_phrase",
+    #     help="My Delete Phrase",
+    # )
+
+    # aro_cluster_parser.add_argument(
+    #     "--aro-cluster-name",
+    #     required=True,
+    #     action="store",
+    #     dest="aro_cluster_name",
+    #     help="ARO Cluster Name",
+    # )
+
+    aro_delete_cluster_parser.add_argument(
+        "--aro-cluster-name",
+        required=True,
+        action="store",
+        dest="aro_cluster_name",
+        help="ARO Cluster Name",
+    )
+
+    aro_delete_cluster_parser.add_argument(
+        "--aro-client-id",
+        required=True,
+        action="store",
+        dest="aro_client_id",
+        help="ARO Client ID",
+    )
+
+    aro_delete_cluster_parser.add_argument(
+        "--aro-tenant-id",
+        required=True,
+        action="store",
+        dest="aro_tenant_id",
+        help="ARO Tenant ID",
+    )
+
+    aro_delete_cluster_parser.add_argument(
+        "--aro-subscription-id",
+        required=True,
+        action="store",
+        dest="aro_subscription_id",
+        help="ARO Subscription ID",
+    )
+
+    aro_delete_cluster_parser.add_argument(
+        "--aro-secret-pwd",
+        required=True,
+        action="store",
+        dest="aro_secret_pwd",
+        help="ARO Secret PWD",
+    )
+
+
+    aro_cluster_manager = AroClusterManager()
+
+    aro_create_cluster_parser.set_defaults(func=aro_cluster_manager.create_aro_cluster)
+
+    aro_delete_cluster_parser.set_defaults(func=aro_cluster_manager.delete_aro_cluster) 
+    args = parser.parse_args(namespace=aro_cluster_manager)
+    if hasattr(args, "func"):
+        args.func()
+
+
+if __name__ == "__main__":
+    main()

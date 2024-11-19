0a42557c68f6fbceefe0f1dc714319523579d0cd
Jorge <jgarciao@users.noreply.github.com>
2024-11-19 09:08:07 +0100

    Backport fix in pipeline path in create_run_from_pipeline_func (releases/2.10.0) (#2048)
    * Fix path in create_run_from_pipeline_func
    
    Signed-off-by: Jorge Garcia Oncins <jgarciao@redhat.com>
    
    * Fix linter issue
    
    Signed-off-by: Jorge Garcia Oncins <jgarciao@redhat.com>
    
    ---------
    
    Signed-off-by: Jorge Garcia Oncins <jgarciao@redhat.com>

--- ods_ci/libs/DataSciencePipelinesKfp.py
+++ ods_ci/libs/DataSciencePipelinesKfp.py
@@ -100,9 +100,7 @@ class DataSciencePipelinesKfp:
         # the current_path will be ods-ci
         if current_path is None:
             current_path = os.getcwd()
-        my_source = self.import_souce_code(
-            f"{current_path}/ods_ci/tests/Resources/Files/pipeline-samples/v2/{source_code}"
-        )
+        my_source = self.import_souce_code(f"{current_path}/tests/Resources/Files/pipeline-samples/v2/{source_code}")
         pipeline = getattr(my_source, fn)
 
         # pipeline_params

f7528508866154d0aa42435c73b83cf2f813d57c
root <root@govind-odh.pokprv.stglabs.ibm.com>
2025-03-11 06:02:29 -0400

    must-gather cleanup

--- ods_ci/tests/Resources/CLI/MustGather/get-must-gather-logs.sh
+++ ods_ci/tests/Resources/CLI/MustGather/get-must-gather-logs.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # Redirecting stdout/stderr of must-gather to a file, as it fills up the
 # process buffer and prevents the script from running further.
+#clean up must-gather.local* 
+for dir in must-gather.local*; do
+    if [ -d "$dir" ]; then
+        echo "Removing existing directory: $dir"
+        rm -rf "$dir"
+    fi
+done
+
 oc adm must-gather --image=quay.io/modh/must-gather@sha256:9d5988f45c3b00ec7fbbe7a8a86cc149a2768c9c47e207694fdb6e87ef44adf3 -- "export OPERATOR_NAMESPACE=${OPERATOR_NAMESPACE};export APPLICATIONS_NAMESPACE=${APPLICATIONS_NAMESPACE}; /usr/bin/gather" &> must-gather-results.txt
 
 if [ $? -eq 0 ]

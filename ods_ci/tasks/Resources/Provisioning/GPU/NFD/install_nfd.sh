3b2320357f64e7a07e17c3b9b41fc9a138074c0a
Pierangelo Di Pilato <pierdipi@redhat.com>
2025-10-06 15:23:50 +0200

    Revert "Increase timeouts for GPU and NFD installation"
    This reverts commit 7e7f47b6d8ddaf2147f562346e4e3edb2666f2b0.
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- ods_ci/tasks/Resources/Provisioning/GPU/NFD/install_nfd.sh
+++ ods_ci/tasks/Resources/Provisioning/GPU/NFD/install_nfd.sh
@@ -6,11 +6,11 @@ NFD_INSTANCE=$NFD_INSTALL_DIR/nfd_deploy.yaml
 
 echo "Installing NFD operator"
 oc apply -f "$NFD_INSTALL_DIR/nfd_operator.yaml"
-oc wait --timeout=20m --for jsonpath='{.status.state}'=AtLatestKnown -n openshift-nfd sub nfd
+oc wait --timeout=3m --for jsonpath='{.status.state}'=AtLatestKnown -n openshift-nfd sub nfd
 
 echo "Installing SR-IOV Network Operator"
 oc apply -f "$NFD_INSTALL_DIR/sriov_operator.yaml"
-oc wait --timeout=20m --for jsonpath='{.status.state}'=AtLatestKnown -n openshift-sriov-network-operator sub sriov-network-operator-subscription
+oc wait --timeout=8m --for jsonpath='{.status.state}'=AtLatestKnown -n openshift-sriov-network-operator sub sriov-network-operator-subscription
 
 ocpVersion=$(oc version --output json | jq '.openshiftVersion' | tr -d '"')
 IFS='.' read -ra ocpVersionSplit <<< "$ocpVersion"

0f06b58be591cd60fea1c92e6649cf1c6f61524c
Pierangelo Di Pilato <pierdipi@redhat.com>
2025-10-06 09:20:04 +0200

    Deploy SR-IOV operator and update ClusterPolicy for RDMA
    for testing llm-d, we might need to use SR-IOV and the Nvidia kernel
    parameters `EnableStreamMemOPs=1` and `PeerMappingOverride=1`.
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- ods_ci/tasks/Resources/Provisioning/GPU/NFD/install_nfd.sh
+++ ods_ci/tasks/Resources/Provisioning/GPU/NFD/install_nfd.sh
@@ -3,9 +3,14 @@ set -e
 
 NFD_INSTALL_DIR="$(dirname "$0")"
 NFD_INSTANCE=$NFD_INSTALL_DIR/nfd_deploy.yaml
+
 echo "Installing NFD operator"
 oc apply -f "$NFD_INSTALL_DIR/nfd_operator.yaml"
 oc wait --timeout=20m --for jsonpath='{.status.state}'=AtLatestKnown -n openshift-nfd sub nfd
+
+echo "Installing SR-IOV Network Operator"
+oc apply -f "$NFD_INSTALL_DIR/sriov_operator.yaml"
+oc wait --timeout=20m --for jsonpath='{.status.state}'=AtLatestKnown -n openshift-sriov-network-operator sub sriov-network-operator-subscription
 
 ocpVersion=$(oc version --output json | jq '.openshiftVersion' | tr -d '"')
 IFS='.' read -ra ocpVersionSplit <<< "$ocpVersion"
@@ -30,3 +35,6 @@ sed -i'' -e "s/<imageUrl>/$imageUrl/g" $NFD_INSTANCE
 # temporary sleep until latest oc binary is available and --for=create is supported
 sleep 10s
 oc apply -f "$NFD_INSTANCE"
+
+echo "Configuring SR-IOV Operator"
+oc apply -f "$NFD_INSTALL_DIR/sriov_network_node_policy.yaml"

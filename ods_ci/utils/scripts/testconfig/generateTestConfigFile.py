c18ad0f6974e9f194ab36fb515c59ade5554913c
Andrej Smigala <asmigala@redhat.com>
2025-10-22 16:29:23 +0200

    Test config generation for OIDC cluster login

--- ods_ci/utils/scripts/testconfig/generateTestConfigFile.py
+++ ods_ci/utils/scripts/testconfig/generateTestConfigFile.py
@@ -11,6 +11,7 @@ from ods_ci.utils.scripts.util import (
     clone_config_repo,
     execute_command,
     oc_login,
+    oc_login_oidc,
     read_yaml,
 )
 
@@ -234,6 +235,8 @@ def generate_test_config_file(
     data["OCP_ADMIN_USER"]["AUTH_TYPE"] = config_data["TEST_CLUSTERS"][test_cluster]["OCP_ADMIN_USER"]["AUTH_TYPE"]
     data["OCP_ADMIN_USER"]["USERNAME"] = config_data["TEST_CLUSTERS"][test_cluster]["OCP_ADMIN_USER"]["USERNAME"]
     data["OCP_ADMIN_USER"]["PASSWORD"] = config_data["TEST_CLUSTERS"][test_cluster]["OCP_ADMIN_USER"]["PASSWORD"]
+    data["CLUSTER_AUTH"] = config_data["TEST_CLUSTERS"][test_cluster]["CLUSTER_AUTH"]
+    data["CLUSTER_OIDC_ISSUER"] = config_data["TEST_CLUSTERS"][test_cluster]["CLUSTER_OIDC_ISSUER"]
     data["SSO"]["USERNAME"] = config_data["SSO"]["USERNAME"]
     data["SSO"]["PASSWORD"] = config_data["SSO"]["PASSWORD"]
     data["RHODS_BUILD"]["PULL_SECRET"] = config_data["RHODS_BUILD"]["PULL_SECRET"]
@@ -321,12 +324,19 @@ def generate_test_config_file(
         data["CUSTOM_MANIFESTS"] = initialize_custom_manifest(custom_manifests)
 
     # Login to test cluster using oc command
-    oc_login(
-        data["OCP_API_URL"],
-        data["OCP_ADMIN_USER"]["USERNAME"],
-        data["OCP_ADMIN_USER"]["PASSWORD"],
-    )
-    print("After oc login")
+    if data["CLUSTER_AUTH"] == "oidc":
+        oc_login_oidc(
+            data["OCP_API_URL"],
+            data["OCP_ADMIN_USER"]["USERNAME"],
+            data["OCP_ADMIN_USER"]["PASSWORD"],
+            data["CLUSTER_OIDC_ISSUER"],
+        )
+    else:
+        oc_login(
+            data["OCP_API_URL"],
+            data["OCP_ADMIN_USER"]["USERNAME"],
+            data["OCP_ADMIN_USER"]["PASSWORD"],
+        )
 
     if bool(set_prometheus_config):
         # Get prometheus token for test cluster

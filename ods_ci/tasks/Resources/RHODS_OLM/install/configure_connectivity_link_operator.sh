29f66e43a2e6a204eda188aee795d76ea8f7431b
Marián Macik <mmacik@redhat.com>
2026-01-26 09:14:50 +0100

    fix: RHOAIENG-45788 - ERROR: Authorino service not found after 300 seconds

--- ods_ci/tasks/Resources/RHODS_OLM/install/configure_connectivity_link_operator.sh
+++ ods_ci/tasks/Resources/RHODS_OLM/install/configure_connectivity_link_operator.sh
@@ -10,3 +10,19 @@ metadata:
   namespace: kuadrant-system
 EOF
 
+echo ">> Waiting for Kuadrant resource to be ready"
+
+# Wait for the Kuadrant resource to be ready, if it is not ready, restart Kuadrant operator and try again
+if oc wait --for=condition=ready kuadrant kuadrant  -n kuadrant-system --timeout=60s 2>/dev/null; then
+    echo ">> Kuadrant resource ready"
+else
+    echo ">> Kuadrant resource NOT ready, most likely due to race condition when Authorino Operator is not available, restarting Kuadrant Operator" >&2
+    oc delete pod -l app=kuadrant -n kuadrant-system
+    echo ">> Waiting once more for Kuadrant resource to become ready after Kuadrant operator restart"
+    if oc wait --for=condition=ready kuadrant kuadrant  -n kuadrant-system --timeout=60s 2>/dev/null; then
+        echo ">> Kuadrant resource ready"
+    else
+        echo ">> ERROR: Kuadrant resource still NOT ready, even after Kuadrant operator restart" >&2
+        exit 1
+    fi
+fi
